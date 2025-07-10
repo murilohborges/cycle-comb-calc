2373010785e711880cf58750a3305acfcbb0d94d
murilo <mhenrique.mb@gmail.com>
2025-07-10 12:40:18 -0300

    correcting type of humidity

--- app/models/gas_turbine.py
+++ app/models/gas_turbine.py
@@ -5,6 +5,6 @@ class GasTurbineInput(BaseModel):
     Variables for gas turbine calculations
   """
   excess_air_percent: float
-  absolute_humidity: float
+  relative_humidity: float
   gas_turbine_efficiency: float
 
