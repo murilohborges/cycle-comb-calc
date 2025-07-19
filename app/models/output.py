915634a1239fdc66404986a2aa63c1a188f210bd
Murilo Borges <129700245+murilohborges@users.noreply.github.com>
2025-07-19 22:20:16 +0000

    adding condeser outputs and all numeric constraints

--- app/models/output.py
+++ app/models/output.py
@@ -1,29 +1,36 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class Output(BaseModel):
   """
     All results of calculations
   """
   # Gas turbine (Brayton Cycle)
-  PCI_fuel: float
-  air_mass_flow: float
-  exaustion_gas_tempature: float
-  exaustion_gas_mass_flow: float
+  PCI_fuel: float = Field(..., gt=0)
+  air_mass_flow: float = Field(..., gt=0)
+  exhaustion_gas_tempature: float = Field(..., gt=0)
+  exhaustion_gas_mass_flow: float = Field(..., gt=0)
+
+  # Condenser (Rankine Cycle)
+  thermal_charge: float = Field(..., gt=0)
+  saturated_water_mass_flow: float = Field(..., gt=0)
+  make_up_water_mass_flow: float = Field(..., gt=0)
+  cooling_water_mass_flow: float = Field(..., gt=0)
+  quality_exhaustion_steam_turbine: float = Field(..., gt=0)
 
   # Heat Recovery Steam Generation (HRSG - Rankine Cycle)
-  high_steam_mass_flow: float
-  medium_steam_mass_flow: float
-  low_steam_mass_flow: float
-  pump_variation_pressure: float
+  high_steam_mass_flow: float = Field(..., gt=0)
+  medium_steam_mass_flow: float = Field(..., gt=0)
+  low_steam_mass_flow: float = Field(..., gt=0)
+  pump_variation_pressure: float = Field(..., gt=0)
 
   # Electrical powers generated
-  net_power_gas_turbine: float
-  gross_power_steam_turbine: float
-  net_power_steam_turbine: float
-  power_consumed_pump: float
-  gross_power_cycle_combined: float
-  net_power_cycle_combined: float
+  net_power_gas_turbine: float = Field(..., gt=0)
+  gross_power_steam_turbine: float = Field(..., gt=0)
+  net_power_steam_turbine: float = Field(..., gt=0)
+  power_consumed_pump: float = Field(..., gt=0)
+  gross_power_cycle_combined: float = Field(..., gt=0)
+  net_power_cycle_combined: float = Field(..., gt=0)
 
   # Efficiencies of Cycle Combined
-  gross_cycle_combined: float
-  net_cycle_combined: float
+  gross_cycle_combined: float = Field(..., gt=0, le=100)
+  net_cycle_combined: float = Field(..., gt=0, le=100)
