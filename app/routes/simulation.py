448c13738fb5e554badc4c53e6ec05b19c1c300c
Murilo Borges <129700245+murilohborges@users.noreply.github.com>
2025-07-26 21:34:48 +0000

    recreate endpoint to simulation

--- app/routes/simulation.py
+++ app/routes/simulation.py
@@ -4,7 +4,6 @@ from ..models.input import Input
 router = APIRouter()
 
 
-@router.get("/simulation/", tags=["Simulation"])
+@router.get("/simulation", tags=["Simulation"])
 async def create_simulation(input: Input):
-
-    return {"testando rota API"}
+    return {"message testing"}
