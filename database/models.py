942500d437be4dfa0ec7a00881dd475e0b917690
Murilo Borges <129700245+murilohborges@users.noreply.github.com>
2025-07-27 00:29:22 +0000

    updating models of database

--- database/models.py
+++ database/models.py
@@ -4,20 +4,22 @@ from datetime import datetime
 
 
 class Substance(SQLModel, table=True):
-  id: Optional[int] = Field(default=None, primary_key=True)
-  name: str
-  molar_mass: float
-  lower_calorific_value: float
-  created_at: datetime = Field(default_factory=datetime.utcnow)
-  is_default: bool
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    molar_mass: float
+    lower_calorific_value: float
+    formula: str
+    cas_number: str
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    is_default: bool
 
 
 class CorrelationSpecificHeat(SQLModel, table=True):
-  id: Optional[int] = Field(default=None, primary_key=True)
-  substance_id: int = Field(foreign_key="substance.id")
-  param_A: float
-  param_B: float
-  param_C: float
-  param_D: float
-  created_at: datetime = Field(default_factory=datetime.utcnow)
-  is_default: bool
+    id: Optional[int] = Field(default=None, primary_key=True)
+    substance_id: int = Field(foreign_key="substance.id")
+    param_A: float
+    param_B: float
+    param_C: float
+    param_D: float
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    is_default: bool
