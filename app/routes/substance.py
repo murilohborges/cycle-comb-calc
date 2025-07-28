5e0fc8970988f36b945c4d7126874c4082a67a8b
Murilo Borges <129700245+murilohborges@users.noreply.github.com>
2025-07-28 00:53:32 +0000

    creating substances route test

---
+++ app/routes/substance.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+from ..models.substance import Substance
+from typing import List
+
+router = APIRouter()
+
+
+@router.get("/substances", tags=["Substances"], response_model=List[Substance])
+async def show_substances():
+    return Substance(
+        name="methane",
+        formula="CH4",
+        cas_number="74-82-8"
+    )
