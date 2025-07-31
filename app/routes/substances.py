ecfb4e5abd9367b6b655acda9026e1ddd41ff5f8
murilo <mhenrique.mb@gmail.com>
2025-07-31 11:02:02 -0300

    editing a test to '/substances' endpoint

--- app/routes/substances.py
+++ app/routes/substances.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI
+from fastapi import APIRouter
 from ..models.substance import Substance
 from typing import List
 
 router = APIRouter()
 
 
-@router.get("/substances", tags=["Substances"], response_model=List[Substance])
+@router.get("/substances", tags=["Substances"], response_model=Substance)
 async def show_substances():
   return Substance(
     name="methane",
