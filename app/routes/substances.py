bc59998649af821d0860c3107212baf0f5d489b5
murilo <mhenrique.mb@gmail.com>
2025-08-01 12:47:55 -0300

    connecting substances route to substances controller

--- app/routes/substances.py
+++ app/routes/substances.py
@@ -1,14 +1,11 @@
 from fastapi import APIRouter
-from ..models.substance import Substance
+from app.models.substance import Substance
+from app.controllers.substances_controller import get_all_substances
 from typing import List
 
 router = APIRouter()
 
 
-@router.get("/substances", tags=["Substances"], response_model=Substance)
+@router.get("/substances", tags=["Substances"], response_model=List[Substance])
 async def show_substances():
-  return Substance(
-    name="methane",
-    formula="CH4",
-    cas_number="74-82-8"
-  )
+  return get_all_substances()
