0cba6f809a71f032a7c4863870f33ed361df6149
murilo <mhenrique.mb@gmail.com>
2025-08-07 12:38:14 -0300

    adding a input test for full_cycles service

--- app/services/full_cycles.py
+++ app/services/full_cycles.py
@@ -11,9 +11,59 @@ FullCycleResult = namedtuple("FullCycleResult", [
     "net_power_cycle_combined", "gross_cycle_combined", "net_cycle_combined"
 ])
 
+# Tuple input test
+FullCycleInput = namedtuple("FullCycleInput", [
+    "methane_molar_fraction_fuel", "ethane_molar_fraction_fuel", "propane_molar_fraction_fuel",
+    "butane_molar_fraction_fuel", "water_molar_fraction_fuel", "carbon_dioxide_molar_fraction_fuel",
+    "hydrogen_molar_fraction_fuel", "nitrogen_molar_fraction_fuel", "fuel_mass_flow",
+    "fuel_input_temperature", "air_input_temperature", "percent_excess_air",
+    "local_atmospheric_pressure", "relative_humity", "gas_turbine_efficiency",
+    "chimney_gas_temperature", "purge_level", "high_steam_level_pressure",
+    "medium_steam_level_pressure", "low_steam_level_pressure", "high_steam_level_temperature", "medium_steam_level_temperature", "low_steam_level_temperature", "high_steam_level_fraction", "medium_steam_level_fraction", "high_steam_level_efficiency", "medium_steam_level_efficiency", "low_steam_level_efficiency", "reductor_generator_set_efficiency", "pump_efficiency", "engine_pump_efficiency", "power_factor_pump_efficiency", "condenser_operation_pressure", "range_temperature_cooling_tower"
+])
+general_input = FullCycleInput(
+    methane_molar_fraction_fuel=87.08,
+    ethane_molar_fraction_fuel=7.83,
+    propane_molar_fraction_fuel=2.94,
+    butane_molar_fraction_fuel=0,
+    water_molar_fraction_fuel=0,
+    carbon_dioxide_molar_fraction_fuel=0.68,
+    hydrogen_molar_fraction_fuel=0,
+    nitrogen_molar_fraction_fuel=1.47,
+    fuel_mass_flow=53064,
+    fuel_input_temperature=25,
+    air_input_temperature=25,
+    percent_excess_air=164.15,
+    local_atmospheric_pressure=1,
+    relative_humity=60,
+    gas_turbine_efficiency=36.78,
+    chimney_gas_temperature=99.7,
+    purge_level=0,
+    high_steam_level_pressure=98.8,
+    high_steam_level_temperature=565,
+    high_steam_level_fraction=70,
+    medium_steam_level_pressure=24,
+    medium_steam_level_temperature=565,
+    medium_steam_level_fraction=15,
+    low_steam_level_pressure=4,
+    low_steam_level_temperature=312.5,
+    high_steam_level_efficiency=87,
+    medium_steam_level_efficiency=91,
+    low_steam_level_efficiency=89,
+    reductor_generator_set_efficiency=98.5,
+    pump_efficiency=75,
+    engine_pump_efficiency=82.5,
+    power_factor_pump_efficiency=0.84,
+    condenser_operation_pressure=0.074,
+    range_temperature_cooling_tower=10
+  )
+
+
 class FullCycle:
   
   def create_full_cycles_combined():
+
+
     result_of_cycles = FullCycleResult(
       PCI_fuel=1.0,
       air_mass_flow=1.0,
