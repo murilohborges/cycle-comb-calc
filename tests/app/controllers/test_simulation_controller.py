518a690f010efdb91189161644c14f5ec508dcef
murilo <mhenrique.mb@gmail.com>
2025-08-07 11:24:52 -0300

    creating test of simulation controller

---
+++ tests/app/controllers/test_simulation_controller.py
@@ -0,0 +1,10 @@
+import pytest
+from app.controllers.simulation_controller import create_simulation
+
+def test_create_simulation_returns_result():
+  """
+  Testing simulation controller returns a result not null
+  """
+  result = create_simulation(input)
+
+  print(result)
