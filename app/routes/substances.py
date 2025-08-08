710c42a22e252f96dc64fedcb0af13673e0b1ce7
murilo <mhenrique.mb@gmail.com>
2025-08-08 17:11:35 -0300

    injecting session db in substances routes

--- app/routes/substances.py
+++ app/routes/substances.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlmodel import Session
+from app.database.session import get_session
 from app.models.substance import Substance
 from app.controllers.substances_controller import get_all_substances
 from typing import List
@@ -7,5 +9,5 @@ router = APIRouter()
 
 
 @router.get("/substances", tags=["Substances"], response_model=List[Substance])
-async def show_substances():
-  return get_all_substances()
+async def show_substances(db: Session = Depends(get_session)):
+  return get_all_substances(db)
