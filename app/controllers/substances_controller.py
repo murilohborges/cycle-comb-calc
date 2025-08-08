11aa76090b5796bd749e0bd3a006c302930f72fe
murilo <mhenrique.mb@gmail.com>
2025-08-08 17:12:23 -0300

    updating db import session

--- app/controllers/substances_controller.py
+++ app/controllers/substances_controller.py
@@ -1,9 +1,6 @@
-from sqlmodel import Session, select
+from sqlmodel import select
 from app.database.models import Substance
-from app.database.engine import engine
 
-def get_all_substances() -> list[Substance]:
-  with Session(engine) as session:
-    statement = select(Substance)
-    results = session.exec(statement).all()
-    return results
+def get_all_substances(db) -> list[Substance]:
+  statement = select(Substance)
+  return db.exec(statement).all()
