e1d3b69b3bf38ddf1c81286ae5dc38ad3d3a2d5c
murilo <mhenrique.mb@gmail.com>
2025-08-11 20:41:43 -0300

    adding Session of database

--- app/routes/simulation.py
+++ app/routes/simulation.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlmodel import Session
+from app.database.session import get_session
 from ..models.input import Input
 from ..models.output import Output
 from ..controllers.simulation_controller import create_simulation
@@ -7,5 +9,5 @@ router = APIRouter()
 
 
 @router.post("/simulation", tags=["Simulation"], response_model=Output)
-async def call_simulation(input: Input):
-  return create_simulation(input)
+async def call_simulation(input: Input, db: Session = Depends(get_session)):
+  return create_simulation(input, db)
