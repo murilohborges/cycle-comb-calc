16ed1532ad75bf5e5254bd24cf52ef6cab1b2fe5
murilo <mhenrique.mb@gmail.com>
2025-08-12 19:44:00 -0300

    changing instance of full_cycles service

--- app/controllers/simulation_controller.py
+++ app/controllers/simulation_controller.py
@@ -1,5 +1,6 @@
-from ..services.full_cycles import FullCycle
+from ..services.full_cycles import FullCycles
 
 def create_simulation(input, db):
-  results = FullCycle.create_full_cycles_combined(input, db)
+  full_cycles = FullCycles(input, db)
+  results = full_cycles.create_full_cycles_combined()
   return results
