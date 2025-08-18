2d6d64620b7ea8a4678d036c97ed0450c1c20ebf
murilo <mhenrique.mb@gmail.com>
2025-08-18 20:14:02 -0300

    refactoring simulation controller to instantiate repositories container

--- app/controllers/simulation_controller.py
+++ app/controllers/simulation_controller.py
@@ -1,6 +1,8 @@
 from ..services.orchestrators.full_cycles import FullCycles
+from ..repositories.repositories_container import RepositoriesContainer
 
 def create_simulation(input, db):
-  full_cycles = FullCycles(input, db)
+  repos = RepositoriesContainer(db)
+  full_cycles = FullCycles(input, repos)
   results = full_cycles.create_full_cycles_combined()
   return results
