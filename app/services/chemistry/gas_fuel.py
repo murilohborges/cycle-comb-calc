5406116945ea5d12afdbd9e642172990ba7ec2b1
murilo <mhenrique.mb@gmail.com>
2025-08-18 15:52:20 -0300

    refactoring code to use SubstanceRepository

--- app/services/chemistry/gas_fuel.py
+++ app/services/chemistry/gas_fuel.py
@@ -2,35 +2,33 @@ from sqlmodel import select
 from ...database.models import Substance
 
 class GasFuel:
-  def __init__(self, input, db):
+  def __init__(self, input, repository):
     self.input = input
-    self.db = db
-
-  # Calculation of PCI of Gas Fuel
-  def LHV_fuel_calc(self):
-    # Validate sum of percents of components in gas fuel
-    fractions = [
+    self.repository = repository
+    self.fractions = [
         (fraction / 100)
         for name, fraction in vars(self.input).items()
         if name.endswith("_molar_fraction_fuel")
       ]
-        
-    sum_percent_components = sum(fractions)
+
+  # Calculation of PCI of Gas Fuel
+  def LHV_fuel_calc(self):
+    # Validate sum of percents of components in gas fuel
+    sum_percent_components = sum(self.fractions)
     
     if sum_percent_components != 1.0:
       raise ValueError(f"Percent invalid: sum = {sum_percent_components}%")
     
     # Getting lhv's and molar mass values of each component
-    statement = select(Substance.lower_calorific_value, Substance.molar_mass)
-    results = self.db.exec(statement).all()
+    results = self.repository.get_all()
 
     # Calculating LHV fuel in Joules/mol
-    LHV_fuel_joule_per_mol = sum(frac * lhv for frac, (lhv, _) in zip(fractions, results))
+    LHV_fuel_joule_per_mol = sum(frac * lhv for frac, (lhv, _) in zip(self.fractions, results))
 
     # Calculating average molar mass of fuel in kmol/kg
-    average_molar_mass_fuel = sum(frac * molar_mass for frac, (_, molar_mass) in zip(fractions, results))
+    average_molar_mass_fuel = sum(frac * molar_mass for frac, (_, molar_mass) in zip(self.fractions, results))
 
     # Calculating LHV fuel in kJ/kg
-    LHV_fuel = LHV_fuel_joule_per_mol/average_molar_mass_fuel
+    LHV_fuel = round(LHV_fuel_joule_per_mol/average_molar_mass_fuel, 2)
 
     return LHV_fuel
