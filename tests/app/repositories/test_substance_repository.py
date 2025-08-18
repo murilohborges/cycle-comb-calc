2ec15cb76038b6ee41c0411d30d8a20addb84c5c
murilo <mhenrique.mb@gmail.com>
2025-08-18 15:50:28 -0300

    creating test for substance_repository

---
+++ tests/app/repositories/test_substance_repository.py
@@ -0,0 +1,27 @@
+import pytest
+from sqlmodel import SQLModel, Session, create_engine
+from app.database.models import Substance
+from app.repositories.substance_repository import SubstanceRepository
+
+@pytest.fixture
+def db_session():
+    engine = create_engine("sqlite:///:memory:")
+    SQLModel.metadata.create_all(engine)
+    with Session(engine) as session:
+        # Inserting fictitious data
+        session.add_all([
+            Substance(name="hydrogen", molar_mass=0.016, lower_calorific_value=50000, formula="H2", cas_number="1333-74-0", is_default=1),
+            Substance(name="methane", molar_mass=0.014, lower_calorific_value=45000, formula="CH4", cas_number="74-82-8", is_default=1)
+        ])
+        session.commit()
+        yield session
+
+def test_get_all(db_session):
+    """
+    Testing get_all() method from SubstanceRepository
+    """
+    repo = SubstanceRepository(db_session)
+    results = repo.get_all()
+    assert len(results) == 2
+    assert results[0] == (50000, 0.016)
+    assert results[1] == (45000, 0.014)
