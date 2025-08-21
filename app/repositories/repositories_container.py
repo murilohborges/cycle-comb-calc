ca76cb8b560863462493bbebc9bba239f23f1e66
murilo <mhenrique.mb@gmail.com>
2025-08-20 21:38:36 -0300

    instatiate icph repo in container

--- app/repositories/repositories_container.py
+++ app/repositories/repositories_container.py
@@ -1,5 +1,8 @@
 from .substance_repository import SubstanceRepository
+from .icph_repository import ICPHRepository
 
 class RepositoriesContainer:
   def __init__(self, db):
     self.substance_repository = SubstanceRepository(db)
+    self.icph_repository = ICPHRepository(db)
+    
