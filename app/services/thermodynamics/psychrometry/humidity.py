77c2b3ed69451b8897f4980821b25f1e3867b351
murilo <mhenrique.mb@gmail.com>
2025-09-03 11:26:55 -0300

    finishing absolute humidity calc

--- app/services/thermodynamics/psychrometry/humidity.py
+++ app/services/thermodynamics/psychrometry/humidity.py
@@ -2,11 +2,11 @@ class Humidity:
   """
   Service class to calculate absolute humidity of the air.
   """
-  def absolute_humidity(saturation_pressure, local_atmospheric_pressure, relative_humidity):
+  def absolute_humidity_calc(self, saturation_pressure, local_atmospheric_pressure, relative_humidity):
     """
     Calculation of absolute humidity in kg of water/kg of dried air
     """
-    steam_pressure = saturation_pressure*relative_humidity
+    steam_pressure = saturation_pressure*(relative_humidity/100)
 
     # Converting local_atmospheric_pressure from atm to bar
     local_atmospheric_pressure = local_atmospheric_pressure * (1.01325)
