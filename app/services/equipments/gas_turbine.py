1f58450585544de8c258f8ebaa3e09b543fbfe47
murilo <mhenrique.mb@gmail.com>
2025-09-05 12:19:38 -0300

    adding input air and combustion gas properties calc methods

--- app/services/equipments/gas_turbine.py
+++ app/services/equipments/gas_turbine.py
@@ -1,6 +1,7 @@
 from ..thermodynamics.heat.icph import ICPH
 from ..chemistry.reactions import Reactions
 from ..chemistry.input_air import InputAir
+from ..chemistry.combustion_gas import CombustionGas
 from ..thermodynamics.psychrometry.humidity import Humidity
 from ..thermodynamics.steam.saturation_parameters import SaturationParameters
 
@@ -12,7 +13,8 @@ class GasTurbine:
     self.substance_repo = substance_repo
     self.icph_repo = icph_repo
     self.reactions = Reactions(self.input, self.gas_fuel.fractions, self.gas_fuel.average_molar_mass_calc(), self.substance_repo)
-    self.input_air = InputAir(self.input, substance_repo)
+    self.input_air = InputAir(self.input, substance_repo, icph_repo)
+    self.combustion_gas = CombustionGas(self.input, substance_repo, icph_repo)
     self.humidity = Humidity()
     self.saturation_parameters = SaturationParameters()
   
@@ -29,18 +31,21 @@ class GasTurbine:
     net_power_GT = (fuel_mass_flow * (LHV_fuel + abs(heat_fuel_input))) / heat_rate
     return net_power_GT
   
-  def input_air_mass_flow(self):
+  def input_air_properties(self):
     reaction_stoichiometric = self.reactions.molar_flow_stoichiometric_calc()
     oxygen_stoichiometric = reaction_stoichiometric['oxygen_stoichiometric']
-    saturation_pressure = self.saturation_parameters.saturation_pressure()
-    absolute_humidity = self.humidity.absolute_humidity(saturation_pressure, self.input.local_atmospheric_pressure, self.input.relative_humidity)
-    air_molar_flow = self.input_air.molar_flow_calc(oxygen_stoichiometric, absolute_humidity)
-    print(air_molar_flow)
-
-    return
+    saturation_pressure = self.saturation_parameters.saturation_pressure(self.input.local_temperature)
+    absolute_humidity = self.humidity.absolute_humidity_calc(saturation_pressure, self.input.local_atmospheric_pressure, self.input.relative_humidity)
+    input_air_properties = self.input_air.input_air_data_calc(oxygen_stoichiometric, absolute_humidity)
+    return input_air_properties
   
-  def combustion_gas_mass_flow():
-    return
+  def combustion_gas_properties(self):
+    reaction_stoichiometric = self.reactions.molar_flow_stoichiometric_calc()
+    gas_fuel_molar_mass = self.gas_fuel.average_molar_mass_calc()
+    input_air = self.input_air_properties()
+    combustion_gas_properties = self.combustion_gas.combustion_gas_data_calc(reaction_stoichiometric, input_air, gas_fuel_molar_mass)
+
+    return combustion_gas_properties
   
   def exhaustion_gas_temp():
     return
