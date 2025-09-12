8b500318d1160584217f197e3fa58ac64a0b8c97
murilo <mhenrique.mb@gmail.com>
2025-09-12 12:48:13 -0300

    creating rankine cycle orchestrator file

---
+++ app/services/orchestrators/rankine_cycle.py
@@ -0,0 +1,21 @@
+from ..thermodynamics.steam.enthalpy import Enthalpy
+from ..thermodynamics.steam.saturation_parameters import SaturationParameters
+from ..equipments.HRSG import HRSG
+from ..equipments.high_steam_turbine import HighSteamTurbine
+
+class RankineCycle:
+  """Service class of all methods and calculations related to Rankine's cycle"""
+  def __init__(self, input, substance_repo, icph_repo):
+    self.input = input
+    self.substance_repo = substance_repo
+    self.icph_repo = icph_repo
+    self.enthalpy = Enthalpy()
+    self.hrsg = HRSG()
+    self.saturation_parameters = SaturationParameters()
+    self.high_steam_turbine = HighSteamTurbine()
+  
+  def HRSG_calc(self):
+    """Calculating operation conditions and properties of HRSG"""
+    hrsg_params = self.hrsg.get_params_operation(self.enthalpy, self.saturation_parameters)
+    return
+  
