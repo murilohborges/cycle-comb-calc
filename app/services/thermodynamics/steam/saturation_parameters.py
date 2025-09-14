f98683e1618389baad56fdbd2accaa75e4d44339
Murilo Borges <129700245+murilohborges@users.noreply.github.com>
2025-09-14 16:00:37 +0000

    correcting result name of methods and add comment in saturation_pressure

--- app/services/thermodynamics/steam/saturation_parameters.py
+++ app/services/thermodynamics/steam/saturation_parameters.py
@@ -20,9 +20,9 @@ class SaturationParameters:
       C = -15.2578
     if pressure < 0.000611 or pressure > 22.1:
       raise ValueError(f"Pressure invalid: out of the range")
-    
-    Tsat = (A + (B / (math.log(pressure) + C))) - 273.15
-    return Tsat
+
+    result = (A + (B / (math.log(pressure) + C))) - 273.15
+    return result
 
   def saturation_pressure(self, temperature):
     """Calculation of saturation pressure(bar) from temperature in celsius"""
@@ -41,20 +41,21 @@ class SaturationParameters:
     A9 = 1.91482E-25
     A10 = -3968.06
     A11 = 39.5735
-    
-    result = (A0 + A1 * (temperature ** 1) + A2 * (temperature ** 2) + A3 * (temperature ** 3) + A4 * (temperature ** 4) + A5 * (temperature ** 5) + A6 * (temperature ** 6) + A7 * (temperature ** 7) + A8 * (temperature ** 8) + A9 * (temperature ** 9)) + (A10 / (temperature - A11))
-    Psat = math.exp(result) * 10
 
-    return Psat
-  
+    result = (A0 + A1 * (temperature ** 1) + A2 * (temperature ** 2) + A3 * (temperature ** 3) + A4 * (temperature ** 4) + A5 * (temperature ** 5) + A6 * (temperature ** 6) + A7 * (temperature ** 7) + A8 * (temperature ** 8) + A9 * (temperature ** 9)) + (A10 / (temperature - A11))
+    # Converting MPa to bar
+    result = math.exp(result) * 10
+
+    return result
+
   def saturation_factor(self, saturation_temp, A, B, C, D, E1, E2, E3, E4, E5, E6, E7):
-    """Calculation of the saturation factor (dimensionless) from the saturation temperature and other parameters. 
+    """Calculation of the saturation factor (dimensionless) from the saturation temperature and other parameters.
     According to the property to be calculated (enthalpy, entropy and others)
     """
     saturation_temp = saturation_temp + 273.15
     Tcr = 647.3
     Tc = (Tcr - saturation_temp) / Tcr
-    
+
     result = A + B * (Tc ** (1 / 3)) + C * (Tc ** (5 / 6)) + D * (Tc ** (7 / 8)) + (E1 * (Tc ** 1) + E2 * (Tc ** 2) + E3 * (Tc ** 3) + E4 * (Tc ** 4) + E5 * (Tc ** 5) + E6 * (Tc ** 6) + E7 * (Tc ** 7))
-    
+
     return result
