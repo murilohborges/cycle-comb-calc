0e72eb9ba92940c867a413184412779df9551434
murilo <mhenrique.mb@gmail.com>
2025-09-15 12:38:33 -0300

    instantiating entropy service

--- app/services/orchestrators/rankine_cycle.py
+++ app/services/orchestrators/rankine_cycle.py
@@ -1,4 +1,5 @@
 from ..thermodynamics.steam.enthalpy import Enthalpy
+from ..thermodynamics.steam.entropy import Entropy
 from ..thermodynamics.steam.saturation_parameters import SaturationParameters
 from ..equipments.HRSG import HRSG
 from ..equipments.high_steam_turbine import HighSteamTurbine
@@ -10,12 +11,13 @@ class RankineCycle:
     self.substance_repo = substance_repo
     self.icph_repo = icph_repo
     self.enthalpy = Enthalpy()
+    self.entropy = Entropy()
     self.hrsg = HRSG()
     self.saturation_parameters = SaturationParameters()
     self.high_steam_turbine = HighSteamTurbine()
   
   def HRSG_calc(self):
     """Calculating operation conditions and properties of HRSG"""
-    hrsg_params = self.hrsg.get_params_operation(self.enthalpy, self.saturation_parameters)
+    hrsg_params = self.hrsg.get_params_operation(self.enthalpy, self.entropy, self.saturation_parameters)
     return
   
