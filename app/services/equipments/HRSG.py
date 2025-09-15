a2d9657a6ca687f1470adc9e1297b0ee8e74f8d0
murilo <mhenrique.mb@gmail.com>
2025-09-15 12:39:33 -0300

    add entropy_calc as param in get_params_operation method

--- app/services/equipments/HRSG.py
+++ app/services/equipments/HRSG.py
@@ -1,7 +1,7 @@
 class HRSG:
   """Service class to calculate enthalpy properties of the Heat Recovery Steam Generator"""
-  def get_params_operation(self, enthalpy_calc, saturation_parameters):
+  def get_params_operation(self, enthalpy_calc, entropy_calc, saturation_parameters):
     """Calculation of mass flows in Heat Recovery Steam Generator (Steam generated, purge, boiler feed water) in kg/h"""
-    enthalpy = enthalpy_calc.saturated_liquid(2, saturation_parameters)
-    print(enthalpy)
+    entropy = entropy_calc.overheated_steam(4, 400, saturation_parameters)
+    print(entropy)
     return
