652d68925e106c5fdde6dbdc369420b56fa4749d
murilo <mhenrique.mb@gmail.com>
2025-09-15 11:24:03 -0300

    adding line to convert saturation temperatue from celsius to kelvin

--- app/services/thermodynamics/steam/enthalpy.py
+++ app/services/thermodynamics/steam/enthalpy.py
@@ -6,6 +6,9 @@ class Enthalpy:
     """Calculate enthalpy of saturated liquid in kJ/kg"""
     saturation_temperature = saturation_parameters.saturation_temperature(pressure)
 
+    # Converting saturation temperature to Kelvin
+    saturation_temperature += 273.15
+
     critical_point_enthalpy = 0
     A = 0
     B = 0
