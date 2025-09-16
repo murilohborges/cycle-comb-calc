d365c7be9fd49daeeb244d12704615c3b8302b5f
murilo <mhenrique.mb@gmail.com>
2025-09-16 12:15:43 -0300

    creatign specific volume calc service

---
+++ app/services/thermodynamics/steam/specific_volume.py
@@ -0,0 +1,26 @@
+import math
+
+class SpecificVolume:
+  """Service class to calculate specific volume property of steam"""
+  def saturated_liquid(self, pressure, saturation_parameters):
+    """Calculate specific volume of saturated liquid in m³/kg"""
+    saturation_temperature = saturation_parameters.saturation_temperature(pressure)
+
+    # Converting saturation temperature to Kelvin
+    saturation_temperature += 273.15
+    
+    critical_point_specific_volume = 0.003155
+    A = 1
+    B = -1.9153882
+    C = 12.015186
+    D = -7.8464025
+    E1 = -3.888614
+    E2 = 2.0582238
+    E3 = -2.0829991
+    E4 = 0.82180004
+    E5 = 0.47549742
+    E6 = 0
+    E7 = 0
+
+    result = saturation_parameters.saturation_factor(saturation_temperature, A, B, C, D, E1, E2, E3, E4, E5, E6, E7) * critical_point_specific_volume
+    return result
