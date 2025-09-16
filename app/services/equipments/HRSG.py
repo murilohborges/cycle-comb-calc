96a6cba474547d7f891ea8103ffb0d2a362fca27
murilo <mhenrique.mb@gmail.com>
2025-09-16 12:16:28 -0300

    adding speficic volume calc as params

--- app/services/equipments/HRSG.py
+++ app/services/equipments/HRSG.py
@@ -1,7 +1,7 @@
 class HRSG:
   """Service class to calculate enthalpy properties of the Heat Recovery Steam Generator"""
-  def get_params_operation(self, enthalpy_calc, entropy_calc, saturation_parameters):
+  def get_params_operation(self, enthalpy_calc, entropy_calc, specific_volume_calc, saturation_parameters):
     """Calculation of mass flows in Heat Recovery Steam Generator (Steam generated, purge, boiler feed water) in kg/h"""
-    entropy = entropy_calc.overheated_steam(4, 400, saturation_parameters)
-    print(entropy)
+    specific_volume = specific_volume_calc.saturated_liquid(1.01325, saturation_parameters)
+    print(specific_volume)
     return
