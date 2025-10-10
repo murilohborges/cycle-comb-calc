5773cbad4819adb899318c468ecfec0ca3098a36
murilo <mhenrique.mb@gmail.com>
2025-10-10 12:42:40 -0300

    refactoring tests with error validations

--- tests/app/services/chemistry/test_gas_fuel.py
+++ tests/app/services/chemistry/test_gas_fuel.py
@@ -1,6 +1,6 @@
 import pytest
 from app.services.chemistry.gas_fuel import GasFuel
-
+from app.utils.errors import LogicConstraintError, ThermodynamicError, DataValidationError
 
 # Fixture parametrizada para frações de combustível
 @pytest.fixture(
@@ -25,19 +25,33 @@ class MockSubstanceRepository:
   def __init__(self):
     self.results = {
       "hydrogen":{'id': 1, 'molar_mass': 0.016, 'lower_calorific_value': 50000},
-      "methane":{'id': 2, 'molar_mass': 0.014, 'lower_calorific_value': 45000}
+      "methane":{'id': 2, 'molar_mass': 0.014, 'lower_calorific_value': 45000},
+      "nitrogen":{'id': 3, 'molar_mass': 0.028, 'lower_calorific_value': 0},
+      "water":{'id': 4, 'molar_mass': 0.018, 'lower_calorific_value': 0}
     }
   def get_all(self):
     return self.results
+  
+class MockInvalidSubstanceRepository:
+  def get_all(self):
+    return 0
 
 class MockICPHRepository:
   def __init__(self):
     self.results = [
         {"param_A": 1.0, "param_B": 2.0, "param_C": 3.0, "param_D": 4.0},
-        {"param_A": 2.0, "param_B": 4.0, "param_C": 6.0, "param_D": 8.0}
+        {"param_A": 2.0, "param_B": 4.0, "param_C": 6.0, "param_D": 8.0},
+        {"param_A": 2.5, "param_B": 3.0, "param_C": 9.0, "param_D": 0.0},
+        {"param_A": 2.7, "param_B": 5.0, "param_C": 0.0, "param_D": 10.0}
       ]
   def get_by_substance_id(self, substance_id):
     return self.results[(substance_id - 1)]
+
+class MockInvalidICPHRepository:
+  def __init__(self):
+    self.results = 0
+  def get_by_substance_id(self, substance_id):
+    return 0
 
 class TestGasFuel:
 
@@ -62,7 +76,7 @@ class TestGasFuel:
       gas_fuel._validate_fractions()
     else:
       # incorrect sum (invalid)
-      with pytest.raises(ValueError):
+      with pytest.raises(LogicConstraintError):
         gas_fuel._validate_fractions()
   
   def test_average_molar_mass(self, mock_input_factory):
@@ -79,6 +93,20 @@ class TestGasFuel:
     expected_molar_mass = (0.3 * 0.016) + (0.7 * 0.014)
 
     assert result == pytest.approx(expected_molar_mass)
+  
+  def test_average_molar_mass_invalid_data(self, mock_input_factory):
+    """Testing avarege molar mass calculation without components data"""
+    fractions = {"hydrogen_molar_fraction_fuel": 30, "methane_molar_fraction_fuel": 70}
+    mock_input = mock_input_factory(**fractions)
+    substance_repo_mock = MockInvalidSubstanceRepository()
+    icph_repom_mock = MockInvalidICPHRepository()
+
+    gas_fuel = GasFuel(mock_input, substance_repo_mock, icph_repom_mock)
+
+    # Checks if Validation Error is raised
+    with pytest.raises(DataValidationError):
+      result = gas_fuel.average_molar_mass_calc()
+
 
   def test_lhv_calculation_with_valid_data(self, mock_input_factory):
     """
@@ -98,6 +126,22 @@ class TestGasFuel:
     expected_lhv = expected_lhv_joule_per_mol / expected_molar_mass
 
     assert result == pytest.approx(expected_lhv, rel=1e-2)
+  
+  def test_lhv_calculation_with_invalid_data(self, mock_input_factory):
+    """
+    Test LHV calculation with invalid data, with only inerts in composition.
+    """
+    fractions = {"nitrogen_molar_fraction_fuel": 80, "water_molar_fraction_fuel": 20}
+    mock_input = mock_input_factory(**fractions)
+    substance_mock_repository = MockSubstanceRepository()
+    icph_mock_repository = MockICPHRepository()
+
+    service = GasFuel(mock_input, substance_mock_repository, icph_mock_repository)
+
+    # Checks if the Validation Error is raised
+    with pytest.raises(ThermodynamicError):
+      service.LHV_fuel_calc()
+    
   
   def test_icph_params_calc(self, mock_input_factory):
     """
