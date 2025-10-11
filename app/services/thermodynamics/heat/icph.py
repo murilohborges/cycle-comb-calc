44e35586d7fa80b1e3d3bf98f07696ff149afb63
Murilo Borges <129700245+murilohborges@users.noreply.github.com>
2025-10-11 21:24:05 +0000

    adding error validation type

--- app/services/thermodynamics/heat/icph.py
+++ app/services/thermodynamics/heat/icph.py
@@ -1,10 +1,12 @@
+from app.utils.errors import DataValidationError
+
 class ICPH:
   """
-  Computational routine called ICPH to calculate sensible 
+  Computational routine called ICPH to calculate sensible
   heat of a flow (fuel, air or combustion gas) in kJ/kg.
   """
   def __init__(self):
-        # Universal Gas Constant in J/(mol.K) 
+        # Universal Gas Constant in J/(mol.K)
         self.R = 8.314462618
   # Calculate heat with icph equation
   def icph_calc_heat(self, icph_params, molar_mass, temp_in, temp_out):
@@ -18,7 +20,7 @@ class ICPH:
 
     # Validate molar mass value
     if (molar_mass <= 0):
-      raise ValueError(f"Molar mass invalid: molar_mass = {molar_mass}")
+      raise DataValidationError(f"Molar mass invalid: molar_mass = {molar_mass}")
 
     heat = (self.R/molar_mass) * ((A*temp_in*(tau - 1)) + ((B/2)*(temp_in**2)*((tau**2)-1)) + ((C/3)*(temp_in**3)*((tau**3)-1)) + ((D/temp_in)*((tau-1)/tau)))
 
