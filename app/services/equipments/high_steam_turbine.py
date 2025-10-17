7331e3e3dae08e49147538f28b3035c42bd9ae9c
murilo <mhenrique.mb@gmail.com>
2025-10-17 12:09:27 -0300

    adding error validation

--- app/services/equipments/high_steam_turbine.py
+++ app/services/equipments/high_steam_turbine.py
@@ -1,8 +1,16 @@
+from app.utils.errors import ThermodynamicError
+
 class HighSteamTurbine:
   """Service class to calculate properties of high pressure steam turbine"""
   def get_params_operation(self, input, saturation_parameters, entropy, enthalpy, secant_method):
     """Calculation of params of operation of High Level Steam Turbine"""
     efficiency = input.high_steam_level_efficiency
+    saturation_temperature = round(saturation_parameters.saturation_temperature(input.high_steam_level_pressure), 2)
+
+    # Before calculating, checks if the temperature of the steam is above saturation, so the secant method can converge
+    # This verification is crucial for the HRSG's calculations, so it is made in this module too
+    if input.high_steam_level_temperature <= saturation_temperature:
+      raise ThermodynamicError(f"The high steam temperature is below saturation ({saturation_temperature}°C)")
 
     # Inlet params
     inlet_entropy = entropy.overheated_steam(input.high_steam_level_pressure, input.high_steam_level_temperature)
